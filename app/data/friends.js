3d433bcf18538ba14cb3c450ca7b7ad579316caa
Brian Hart <brianhart@dhcp-v088-235.mobile.uci.edu>
2017-10-11 20:58:09 -0700

    not sure

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,11 @@ var friendArray = [
     "name": "Brian",
     "photo": "https://i.pinimg.com/736x/77/05/be/7705bea9f596b89bd3e4a5a087b6f66d--quokka-australian-animals.jpg",
     "scores": "2,5,3,4,1,5,3,2,1,4"
+  },
+  {
+    "name": "Donald",
+    "photo": "http://static4.businessinsider.com/image/56c640526e97c625048b822a-480/donald-trump.jpg",
+    "scores": "1,5,2,4,3,5,4,4,2,1"
   }
 ];
 
